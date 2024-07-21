a3a532819b294e91730df3e2b0a9dd872c966660
ValentinCassayre <valentin.cassayre@gmail.com>
2024-07-21 17:14:52 +0200

    Converting to keyboard input

--- socket-receiver.py
+++ socket-receiver.py
@@ -1,5 +1,6 @@
 import socket
-import struct  # Import struct module to unpack binary data
+import struct
+import pyautogui
 
 PORT = 8888
 
@@ -18,6 +19,30 @@ def get_private_ip():
         return None
 
 
+def num_to_action(key_press):
+    binary_string = "{0:016b}".format(key_press)[::-1]
+    key_press_list = [digit == "1" for digit in binary_string]
+    print(binary_string)
+    actions = [None] * 16
+    actions[0] = pyautogui.press("a")  # A
+    actions[1] = pyautogui.press("b")  # B
+    actions[2] = pyautogui.press("enter")  # start
+    actions[3] = pyautogui.press("escape")  # select
+    actions[4] = pyautogui.press("right")  # ->
+    actions[5] = pyautogui.press("left")  # <-
+    actions[6] = pyautogui.press("up")  # /\
+    actions[7] = pyautogui.press("down")  # \/
+    actions[8] = pyautogui.press("l")  # L
+    actions[9] = pyautogui.press("r")  # R
+    actions[10] = pyautogui.press("x")  # X
+    actions[11] = pyautogui.press("y")  # Y
+    actions[12] = pyautogui.click(button="left")
+
+    for index, control in enumerate(key_press_list):
+        if control and actions[index]:
+            actions[index]()
+
+
 def receive_from_ds(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((ip, port))
@@ -32,8 +57,11 @@ def receive_from_ds(ip, port):
         if not data:
             break
         # Unpack the received bytes into an integer
-        count = struct.unpack('<I', data)[0]  # Use '<I' for little-endian, 'I' is for unsigned int
-        print(f"Received: {count}")
+        key_press = struct.unpack("<I", data)[
+            0
+        ]  # Use '<I' for little-endian, 'I' is for unsigned int
+        print(f"Received: {key_press}")
+        num_to_action(key_press)
 
     conn.close()
 
@@ -41,4 +69,4 @@ def receive_from_ds(ip, port):
 if __name__ == "__main__":
     ip = get_private_ip()
     print(f"Private IP Address: {ip}")
-    receive_from_ds(ip , PORT)
+    receive_from_ds(ip, PORT)
