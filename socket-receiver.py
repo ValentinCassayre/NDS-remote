02c3fe52a0e9aba2403b2642d4c8ded3452b1945
ValentinCassayre <valentin.cassayre@gmail.com>
2024-07-21 17:59:33 +0200

    Convert to controler input

--- socket-receiver.py
+++ socket-receiver.py
@@ -1,6 +1,6 @@
 import socket
 import struct
-import pyautogui
+
 
 PORT = 8888
 
@@ -19,10 +19,52 @@ def get_private_ip():
         return None
 
 
-def num_to_action(key_press):
+def int_to_bools(key_press):
     binary_string = "{0:016b}".format(key_press)[::-1]
     key_press_list = [digit == "1" for digit in binary_string]
+
     print(binary_string)
+    return key_press_list
+
+
+def num_to_action_gamepad(key_press):
+    import vgamepad as vg
+
+    key_press_list = int_to_bools(key_press)
+
+    gamepad = vg.VX360Gamepad()
+
+    actions = [None] * 16
+    actions[0] = vg.XUSB_BUTTON.XUSB_GAMEPAD_A  # A
+    actions[1] = vg.XUSB_BUTTON.XUSB_GAMEPAD_B  # B
+    actions[2] = vg.XUSB_BUTTON.XUSB_GAMEPAD_START  # start
+    actions[3] = vg.XUSB_BUTTON.XUSB_GAMEPAD_BACK  # select
+    actions[4] = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_RIGHT  # ->
+    actions[5] = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_LEFT  # <-
+    actions[6] = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_UP  # /\
+    actions[7] = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_DOWN  # \/
+    actions[8] = vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER  # L
+    actions[9] = vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER  # R
+    actions[10] = vg.XUSB_BUTTON.XUSB_GAMEPAD_X  # X
+    actions[11] = vg.XUSB_BUTTON.XUSB_GAMEPAD_Y  # Y
+    actions[12] = vg.XUSB_BUTTON.XUSB_GAMEPAD_GUIDE  # click on screen
+
+    for index, control in enumerate(key_press_list):
+        if actions[index] is None:
+            continue
+        if control:
+            gamepad.press_button(button=actions[index])
+        else:
+            gamepad.release_button(button=actions[index])
+
+    gamepad.update()
+
+
+def num_to_action_keyboard(key_press):
+    import pyautogui
+
+    key_press_list = int_to_bools(key_press)
+
     actions = [None] * 16
     actions[0] = pyautogui.press("a")  # A
     actions[1] = pyautogui.press("b")  # B
@@ -36,7 +78,7 @@ def num_to_action(key_press):
     actions[9] = pyautogui.press("r")  # R
     actions[10] = pyautogui.press("x")  # X
     actions[11] = pyautogui.press("y")  # Y
-    actions[12] = pyautogui.click(button="left")
+    actions[12] = pyautogui.click(button="left")  # click on screen
 
     for index, control in enumerate(key_press_list):
         if control and actions[index]:
@@ -61,7 +103,7 @@ def receive_from_ds(ip, port):
             0
         ]  # Use '<I' for little-endian, 'I' is for unsigned int
         print(f"Received: {key_press}")
-        num_to_action(key_press)
+        num_to_action_gamepad(key_press)
 
     conn.close()
 
